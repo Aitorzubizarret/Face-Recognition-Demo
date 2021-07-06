d84821eb7c8aec0e8cc9f6d30a9f4d5162d2c873
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-07-06 23:55:30 +0200

    Added a title in the navbar of the main view controller.

--- Face-Recognition-Demo/Controllers/MainViewController.swift
+++ Face-Recognition-Demo/Controllers/MainViewController.swift
@@ -23,7 +23,7 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.title = "Face Recognition Demo"
     }
     
     ///
